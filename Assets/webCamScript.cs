30cc14673f3e9a51b864761c2ff394c9d354ea72
nibraaska <nnibraas@gmail.com>
2018-12-21 00:36:41 -0500

    Added Bullet

--- Assets/webCamScript.cs
+++ Assets/webCamScript.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class webCamScript : MonoBehaviour
 {
     public GameObject webCameraPlane;
+    public Button fireButton;
     
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,19 @@ public class webCamScript : MonoBehaviour
         WebCamTexture webCameraTexture = new WebCamTexture();
         webCameraPlane.GetComponent<MeshRenderer>().material.mainTexture = webCameraTexture;
         webCameraTexture.Play();
+        
+        fireButton.onClick.AddListener (OnButtonDown);
+    }
 
+    void OnButtonDown(){
+        GameObject bullet = Instantiate(Resources.Load("bullet", typeof(GameObject))) as GameObject;
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        bullet.transform.rotation = Camera.main.transform.rotation;
+        bullet.transform.position = Camera.main.transform.position;
+        rb.AddForce(Camera.main.transform.forward * 500f);
+        Destroy (bullet, 3);
+
+        GetComponent<AudioSource> ().Play ();
     }
 
     // Update is called once per frame
